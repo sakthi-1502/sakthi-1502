4638ec93ffba5d26da6a28583b3bbce5ed74d524
Sakthi Kumar M <97774596+sakthi-1502@users.noreply.github.com>
2022-07-15 13:25:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* var typed = new Typed(".typing-1",{
+    strings: [
+        "Software Developer", "Dot Net Developer", "Web Developer"
+    
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+*/
